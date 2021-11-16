80df6c49647184b6299d21e8e035f590df826270
atsushi.takano <atsushi.takano@every.tv>
2021-11-16 15:06:46 +0900

    ボタンとリスナーの実装

--- app/src/main/java/me/aluceps/practicebottomsheetdialog/MainActivity.kt
+++ app/src/main/java/me/aluceps/practicebottomsheetdialog/MainActivity.kt
@@ -1,11 +1,18 @@
 package me.aluceps.practicebottomsheetdialog
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.aluceps.practicebottomsheetdialog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.button.setOnClickListener {
+        }
     }
 }
